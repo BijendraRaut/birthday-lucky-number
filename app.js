5a3504d86d61ed6c3c8fc4f8da36ebbfd6546ee1
Bijendra Raut <bijendraghvm@gmail.com>
2023-05-03 21:42:41 +0530

    JS part done

---
+++ app.js
@@ -0,0 +1,32 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumber = document.querySelector("#calculate-number");
+const outputText = document.querySelector("#output-container");
+
+checkNumber.addEventListener("click", checkBirthdayLucky);
+
+function checkBirthdayLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateDate(dob);
+  if (sum && luckyNumber) comapareValue(sum, luckyNumber.value);
+  else {
+    return (outputText.innerHTML = "Enter the DOB and lucky number first");
+  }
+}
+
+function comapareValue(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    outputText.innerText = "Luck number 🙂";
+  } else {
+    outputText.innerText = "Not Lucky Number 🥹";
+  }
+}
+
+function calculateDate(dob) {
+  dob = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let i = 0; i < dob.length; i++) {
+    sum += Number(dob[i]);
+  }
+  return sum;
+}
